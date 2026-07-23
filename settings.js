c81f5d95d534cc400b30384d57e231db3d209b7e
rama-1-41 <obangaryan41@gmail.com>
2026-07-23 20:42:56 +0300

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: 'RAMA-XMD',
+  author: '‎',
+  botName: "RAMA-XMD",
+  botOwner: 'mr presenter', // Your name
+  ownerNumber: '254769769295', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group plugins and automating tasks.",
+  version: "3.0.7",
+  updateZipUrl: "https://github.com/rama-1-41/RAMA-XMD",
+};
+
+module.exports = settings;
